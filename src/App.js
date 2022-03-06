fbb670279d384f5e551dcdb3d3ba5137643f3246
Azad YILDIZ <azad.yildiz119@gmail.com>
2022-03-06 21:06:48 +0300

    build app frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from "./components/Form"
+import List from "./components/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [{ id: 0, body: " Learn React ❤️", isDone: false }]
+    }
+  }
+
+  addItems = (item) => {
+    this.setState({
+      items: [...this.state.items, item],
+    })
+  }
+  clearItems = () => {
+    this.setState({ items: [] })
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='todo-container'>
+          <Form addItems={this.addItems} />
+          <List data={this.state.items} />
+          <div className='footer'>
+            <button onClick={() => { this.clearItems() }}>Clear All</button>
+          </div>
+        </div>
+      </div >
+    )
+  }
 }
-
-export default App;
